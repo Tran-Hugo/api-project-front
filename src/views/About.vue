e3dabeae800a4a5614ae6132e7b869278b6a0a2f
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-09-21 17:00:26 +0200

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <img alt="Vue logo" class="" src="../assets/logo.png">
+    <div class="container">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" v-model="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" v-model="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+  </div>
+  <button type="submit" @click.prevent="connectUser" class="btn btn-primary">Submit</button>
+</form>
+  </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  data(){
+    return{
+      email:'',
+      password:''
+    }
+  },
+
+  methods:{
+    connectUser(){
+
+                this.user = {
+                    "username": this.email,
+                    "password": this.password
+                }
+
+                // console.log(this.user)
+
+                axios.post('http://127.0.0.1:8000/api/login',this.user,{ withCredentials: true })
+                    .then(res=>{console.log(res)})
+
+            }
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" class="" src="../assets/logo.png">
+    <div class="container">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" v-model="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" v-model="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+  </div>
+  <button type="submit" @click.prevent="createUser" class="btn btn-primary">Submit</button>
+</form>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      email:'',
+      password:''
+    }
+  },
+
+  methods:{
+    createUser(){
+
+                this.user = {
+                    "email": this.email,
+                    "password": this.password
+                }
+
+                // console.log(this.user)
+
+                axios.post('http://127.0.0.1:8000/api/users',this.user)
+                    .then(res=>{console.log(res)})
+
+            }
   }
+
 }
 </script>
